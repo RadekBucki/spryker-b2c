1d72033c54eacaf2583c2bce5b0a6b72d829d03b
Radoslaw Bucki <radoslaw.bucki@accenture.com>
2023-01-13 15:19:23 +0100

    Add planet edit buttons

--- src/Pyz/Zed/Planet/Communication/Table/PlanetTable.php
+++ src/Pyz/Zed/Planet/Communication/Table/PlanetTable.php
@@ -4,11 +4,16 @@ namespace Pyz\Zed\Planet\Communication\Table;
 
 use Orm\Zed\Planet\Persistence\Map\PyzPlanetTableMap;
 use Orm\Zed\Planet\Persistence\PyzPlanetQuery;
+use Spryker\Service\UtilText\Model\Url\Url;
 use Spryker\Zed\Gui\Communication\Table\AbstractTable;
 use Spryker\Zed\Gui\Communication\Table\TableConfiguration;
 
 class PlanetTable extends AbstractTable
 {
+    public const ACTION = 'action';
+    public const UPDATE_PLANET_URL = '/planet/edit';
+    public const PARAM_ID_PLANET = 'id-planet';
+
     /** @var \Orm\Zed\Planet\Persistence\PyzPlanetQuery */
     private PyzPlanetQuery $planetQuery;
 
@@ -29,7 +34,8 @@ class PlanetTable extends AbstractTable
     {
         $config->setHeader([
             PyzPlanetTableMap::COL_NAME             => 'Planet name',
-            PyzPlanetTableMap::COL_INTERESTING_FACT => 'Interesting fact'
+            PyzPlanetTableMap::COL_INTERESTING_FACT => 'Interesting fact',
+            static::ACTION                          => static::ACTION,
         ]);
         $config->setSortable([
             PyzPlanetTableMap::COL_NAME,
@@ -48,6 +54,32 @@ class PlanetTable extends AbstractTable
      */
     protected function prepareData(TableConfiguration $config): array
     {
-        return $this->runQuery($this->planetQuery, $config);
+        $planetDataItems = $this->runQuery($this->planetQuery, $config);
+        $planetTableRows = [];
+        foreach ($planetDataItems as $planetDataItem) {
+            $planetTableRows[] = [
+                PyzPlanetTableMap::COL_NAME             => $planetDataItem[PyzPlanetTableMap::COL_NAME],
+                PyzPlanetTableMap::COL_INTERESTING_FACT => $planetDataItem[PyzPlanetTableMap::COL_INTERESTING_FACT],
+                static::ACTION                          => $this->createActionButtons($planetDataItem)
+            ];
+        }
+        return $planetTableRows;
+    }
+
+    /**
+     * @param array $planetDataItem
+     *
+     * @return array
+     */
+    private function createActionButtons(array $planetDataItem)
+    {
+        $urls = [];
+        $urls[] = $this->generateEditButton(
+            Url::generate(static::UPDATE_PLANET_URL, [
+                static::PARAM_ID_PLANET => $planetDataItem[PyzPlanetTableMap::COL_ID_PLANET],
+            ]),
+            'Edit'
+        );
+        return $urls;
     }
 }
