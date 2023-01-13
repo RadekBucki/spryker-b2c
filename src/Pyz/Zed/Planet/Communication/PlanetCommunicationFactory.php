e884f2ce2734d5c6303b267fe758c0a6e92c175c
Radoslaw Bucki <radoslaw.bucki@accenture.com>
2023-01-13 09:54:05 +0100

    Add create planet form

--- src/Pyz/Zed/Planet/Communication/PlanetCommunicationFactory.php
+++ src/Pyz/Zed/Planet/Communication/PlanetCommunicationFactory.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Pyz\Zed\Planet\Communication;
 
+use Generated\Shared\Transfer\PlanetTransfer;
 use Orm\Zed\Planet\Persistence\PyzPlanetQuery;
+use Pyz\Zed\Planet\Communication\Form\PlanetForm;
 use Pyz\Zed\Planet\Communication\Table\PlanetTable;
 use Pyz\Zed\Planet\PlanetDependencyProvider;
 use Spryker\Zed\Kernel\Communication\AbstractCommunicationFactory;
+use Symfony\Component\Form\FormInterface;
 
 /**
  * @method \Pyz\Zed\Planet\PlanetConfig getConfig()
@@ -30,5 +33,20 @@ class PlanetCommunicationFactory extends AbstractCommunicationFactory
     private function getPlanetPropelQuery(): PyzPlanetQuery
     {
         return $this->getProvidedDependency(PlanetDependencyProvider::QUERY_PLANET);
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\PlanetTransfer|null $planetTransfer
+     * @param array                                          $options
+     *
+     * @return \Symfony\Component\Form\FormInterface
+     */
+    public function createPlanetForm(?PlanetTransfer $planetTransfer = null, array $options = []): FormInterface
+    {
+        return $this->getFormFactory()->create(
+            PlanetForm::class,
+            $planetTransfer,
+            $options
+        );
     }
 }
